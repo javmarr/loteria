f32951aca9fd71efa6995890d09ef2dec857e07c
javmarr <j_marroquin@live.com>
2016-08-18 11:13:56 -0500

    cleaned up "Space Wars" files

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/game/:num', function(req, res, next) {
-  console.log(req.params.num);
-  res.render('game', { gameFile: ('game'+req.params.num+'.js')});
-});
-
 module.exports = router;
