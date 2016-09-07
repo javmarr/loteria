7d0d29e210a6877dd4806807d1e6b4394e5dc127
javmarr <j_marroquin@live.com>
2016-09-07 00:10:05 -0500

    fixed ipaddress to work with other devices on the LAN

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ function normalizePort(val) {
   return false;
 }
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var port = normalizePort(process.env.OPENSHIFT_NODEJS_PORT || 3000);
 
 function terminator(signal) {
