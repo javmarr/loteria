2dcb26ddc7f56bc882de3a431908dcc28829b577
teotzin <125766354+teotzin@users.noreply.github.com>
2023-04-05 01:47:02 +0200

    loginnew

--- setup-passport.js
+++ setup-passport.js
@@ -7,7 +7,9 @@ var strategy = new Auth0Strategy({
     domain:       process.env.DOMAIN,
     clientID:     process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL:  process.env.CALLBACK_URL
+    callbackURL:  process.env.CALLBACK_URL,
+    baseURL: 'https://loteriamujeres.herokuapp.com',
+    issuerBaseURL: 'https://dev-qauothmtu2dhfuyy.us.auth0.com'
   }, function(accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
     // extraParams.id_token has the JSON Web Token
