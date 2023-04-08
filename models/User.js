8979f01ef6f654336ca1b32f309328829352852f
teotzin <125766354+teotzin@users.noreply.github.com>
2023-04-08 21:48:32 +0200

    xx

--- models/User.js
+++ models/User.js
@@ -2,14 +2,14 @@ var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
   userID: {type: String, unique: true},
-  games: [{
+  games: [({
     gameID: {type: String, required: true},
     gameName: {type: String, required: true},
     boardLayout: {type: String, required: true},
     turn: Number,
     deck: Array,
     boards: Array
-  }]
+  })]
 });
 
 module.exports = mongoose.model('User', UserSchema);
