1ed563eff4364e4d666c938709d494ac9f89d0b8
teotzin <125766354+teotzin@users.noreply.github.com>
2023-04-14 00:46:56 +0200

    xx

--- public/javascripts/src/loteria.js
+++ public/javascripts/src/loteria.js
@@ -8,7 +8,7 @@ loteria.prototype = {
   },
 
   create: function() {
-    this.game.stage.backgroundColor = "#000000";
+    this.game.stage.backgroundColor = "#ffffff";
 
     // card = this.game.add.group(); // card containing image and border
     // cardBorder = card.create(0, 0, 'border');
