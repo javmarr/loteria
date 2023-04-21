7bfaf775ec9948f4bc833ea78c1ff42dae57d0e3
teotzin <125766354+teotzin@users.noreply.github.com>
2023-04-21 22:01:44 +0200

    xx

--- public/javascripts/src/gameOverDealer.js
+++ public/javascripts/src/gameOverDealer.js
@@ -8,9 +8,7 @@ gameOverDealer.prototype = {
   create: function() {
   	var gameOverTitle = gameDealer.add.sprite(160, 160, "gameover");
     gameOverTitle.anchor.setTo(0.5, 0.5);
-    var playButton = gameDealer.add.button(160, 320, "play", this.playTheGame, this);
-	playButton.style = {button	};
-  	playButton.anchor.setTo(0.5, 0.5);
+    
 	},
 
 	playTheGame: function() {
