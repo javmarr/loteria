9defda60c76ac87774fd0699ee5f0eb6223e6a40
teotzin <125766354+teotzin@users.noreply.github.com>
2023-04-21 13:21:06 +0200

    xx

--- app.js
+++ app.js
@@ -93,4 +93,17 @@ app.use(function(req, res, next) {
   });
 
 
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+
 module.exports = app;
