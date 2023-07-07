97088ab3edb89e90dbeb4a3fc83d45861a25b50a
teotzin <125766354+teotzin@users.noreply.github.com>
2023-07-07 21:07:00 +0200

    xx

--- public/javascripts/src/loteria.js
+++ public/javascripts/src/loteria.js
@@ -25,7 +25,51 @@ loteria.prototype = {
     groupLoteria.align(4, 4, 210, 330);
     groupLoteria.scale.set(.9, .9);
     groupLoteria.alive=true;
+    groupLoteria.inputEnabled = true;
+    window.addEventListener('message', function(event) 
+   {
+    var message = event.data;
+    console.log('message: ' + message.type + ' ' + message.data);
+    if(message.type === 'cardNumber') 
+    {
+      
+      if(message.data <=50 && message.data>0) {
+        var iIndex = 0;
+        groupLoteria.forEach(function(sprite) {
+            console.log('IMAGE: ' + imageBoard[iIndex]);  
+            console.log('card_'+message.data+'.png');
+            if(imageBoard[iIndex] === 'card_'+message.data+'.png')
+            {
+              console.log('!!!!!SUSTITUYENDO.!!!!!!!!!!!!!!!!: ' + imageBoard[iIndex]);  
+              sprite.loadTexture('cartaCoinci', 'cartaCoinci.png');
+            }
+        iIndex++;
+        });
+      }
+     else  {
+        console.log('Game over');
+        //gameLoteria.state.start('GameOver', true, false, 51);
+      }
+    }
+  })} ,
+
+  update: function() {
+    gameLoteria.stage.backgroundColor = "#BA97FF";
    
+    // card = this.game.add.group(); // card containing image and border
+    // cardBorder = card.create(0, 0, 'border');
+    // cardImage = card.create(cardBorder.x+30, cardBorder.y+32, 'loteria', 'card_' + cardNumber + '.png');
+    console.log('imageboard in phaser: ' + imageBoard);
+
+    groupLoteria = gameLoteria.add.group();
+    //groupLoteria.createMultiple(1, 'loteria', imageBoard, true);
+    groupLoteria.createMultiple(1, 'loteria', imageBoard, true, index => {
+      return imageBoard[index];
+    });
+    groupLoteria.align(4, 4, 210, 330);
+    groupLoteria.scale.set(.9, .9);
+    groupLoteria.alive=true;
+    groupLoteria.inputEnabled = true;
     window.addEventListener('message', function(event) 
    {
     var message = event.data;
@@ -52,4 +96,8 @@ loteria.prototype = {
       }
     }
   })}
-  }
+};
+
+
+
+  
