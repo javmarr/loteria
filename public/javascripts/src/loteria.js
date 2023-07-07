48eca80f0be023df0ccc8e6e3eba309d6956717a
teotzin <125766354+teotzin@users.noreply.github.com>
2023-07-07 21:21:02 +0200

    xx

--- public/javascripts/src/loteria.js
+++ public/javascripts/src/loteria.js
@@ -26,6 +26,14 @@ loteria.prototype = {
     groupLoteria.scale.set(.9, .9);
     groupLoteria.alive=true;
     groupLoteria.inputEnabled = true;
+    
+    //q:when is this event calles?
+    //a:when the user clicks on the card
+    //q:why is not working? 
+    //a:because the event is not called when the user clicks on the card, but when the user clicks on the screen
+    groupLoteria.events.onInputUp.add(this.changeCard,this);
+    console.log("\n\n\n\n\nOn loteria (gameID) " + gameID);
+    
     window.addEventListener('message', function(event) 
    {
     var message = event.data;
@@ -53,34 +61,6 @@ loteria.prototype = {
     }
   })} ,
 
-  update: function() {
-    console.log('update loteria');
-    window.addEventListener('message', function(event) 
-   {
-    var message = event.data;
-    console.log('message: ' + message.type + ' ' + message.data);
-    if(message.type === 'cardNumber') 
-    {
-      
-      if(message.data <=50 && message.data>0) {
-        var iIndex = 0;
-        groupLoteria.forEach(function(sprite) {
-            console.log('IMAGE: ' + imageBoard[iIndex]);  
-            console.log('card_'+message.data+'.png');
-            if(imageBoard[iIndex] === 'card_'+message.data+'.png')
-            {
-              console.log('!!!!!SUSTITUYENDO.!!!!!!!!!!!!!!!!: ' + imageBoard[iIndex]);  
-              sprite.loadTexture('cartaCoinci', 'cartaCoinci.png');
-            }
-        iIndex++;
-        });
-      }
-     else  {
-        console.log('Game over');
-        //gameLoteria.state.start('GameOver', true, false, 51);
-      }
-    }
-  })}
 };
 
 
