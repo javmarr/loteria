5c6ffe538daa15e8523fa027c2b1b36fb1a82145
teotzin <125766354+teotzin@users.noreply.github.com>
2023-07-08 21:25:06 +0200

    xxx

--- public/javascripts/src/loteriaDealer.js
+++ public/javascripts/src/loteriaDealer.js
@@ -11,6 +11,7 @@ loteriaDealer.prototype = {
 
   create: function() {
     //what color is the background? 0xRRGGBB  0x000000 = black  0xffffff = white #ddd 
+    var manager = null;
     gameDealer.stage.backgroundColor = "#BA97FF";
     console.log('cardNumber: ' + cardNumber);
 
