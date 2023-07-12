0e07b2e96985f760d4abae292bade7356f224a3b
teotzin <125766354+teotzin@users.noreply.github.com>
2023-07-12 09:48:54 +0200

    xxx

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ var sslRedirect = require('heroku-ssl-redirect');
 var express = require('express');
 var app = express();
  
-app.use(sslRedirect());
+app.use(sslRedirect);
  
 app.get('/', function(req, res){
   res.send('hello world');
