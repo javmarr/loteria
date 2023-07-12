f8b1b3a4f60587dcf495afe2692e9c52889a103f
teotzin <125766354+teotzin@users.noreply.github.com>
2023-07-12 09:16:54 +0200

    xx

--- app.js
+++ app.js
@@ -78,6 +78,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+var bodyParser = require('body-parser');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
