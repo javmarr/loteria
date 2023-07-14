d124e2355bc11374be834afede7b1036535aa811
teotzin <125766354+teotzin@users.noreply.github.com>
2023-07-14 07:13:56 +0200

    xx

--- public/javascripts/src/loteria.js
+++ public/javascripts/src/loteria.js
@@ -59,7 +59,48 @@ loteria.prototype = {
        var turn = message.turn;
       console.log('turn: ' + turn);
       if(turn>50) 
-        gameLoteria.state.start('GameOver', true, false, 51);
+      {
+        endRoundConfetti();
+      }
+        //gameLoteria.state.start('GameOver', true, false, 51);
   });
   } 
 }
+
+var emitters = [];
+
+
+function endRoundConfetti() {
+    this.createEmitter();
+     this.confettiExplosion();
+  }
+
+ function createEmitter() {
+    const playRect = {x: .9, y: .9, width: 600, height: 800};
+    
+    for (let i = 0; i < 5; i++) {
+      const randomX = (playRect.x * 1.25) + Math.random() * (playRect.width * 0.75);
+      const randomY = (playRect.y * 1.25) + Math.random() * (playRect.height * 0.75);
+
+      emitters.push(gameLoteria.add.emitter(randomX, randomY));
+      emitters[i].makeParticles('confetti1', ["confetti2", "confetti3", "confetti4"]);
+      emitters[i].setSize(playRect.height / 2, playRect.height / 2);
+      emitters[i].gravity = 0;
+      emitters[i].setAlpha(0, 1, 1000, Phaser.Easing.Linear.None, true);
+      emitters[i].setRotation(90, 180);
+      emitters[i].setScale(
+        0.2 / .9,
+        0.4 / .9,
+        0.2 / .9,
+        0.4 / .9
+      );
+      emitters[i].setXSpeed(-300 / .9, 300 / .9);
+      emitters[i].setYSpeed(-300 / .9, 300 / .9);
+    }
+  }
+
+  function confettiExplosion() {
+    emitters.forEach((emitter) => {
+      emitter.start(true, 2000, null, 10);
+    });
+  }
